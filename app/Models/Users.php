9c5f74fd3b92fb83934b5f5aabe09fc0b182fb06
Murkiewater <kekys492@gmail.com>
2026-03-16 23:21:05 +0500

    added sanctumУ

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -5,15 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Users extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     protected $fillable = [
         'full_name',
         'email',
         'password',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
     ];
 
     public function postsInGroups()
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GroupsControllerApi;
 use App\Http\Controllers\UsersControllerApi;
 use App\Http\Controllers\PostInGroupsControllerApi;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,14 @@ Route::get('/users/{id}', [UsersControllerApi::class, 'show']);
 
 Route::get('/post-in-groups', [PostInGroupsControllerApi::class, 'index']);
 Route::get('/post-in-groups/{id}', [PostInGroupsControllerApi::class, 'show']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    Route::middleware('auth:sanctum')->get('/logout', [AuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->get('/groups', [GroupsControllerApi::class, 'index']);   
+});
